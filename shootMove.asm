dfaf27125fad316d0fc32aedc5c75eebc44c193d
Александр <62289323+busyhedg03@users.noreply.github.com>
2022-04-18 01:11:19 +0700

    Обновления по стрельбе
    Код работает и сдвигает, но нужно зациклить

--- shootMove.asm
+++ shootMove.asm
@@ -1,28 +1,26 @@
 asect 0
-ldi r0,arr
+ldi r0,0x3c
 ldi r2,0
 ldi r3, 4
-
 while
-	tst r3
+tst r3
 stays nz
-	if
-	ld r0, r1
-	shr r1
-	push r3
-	ldi r3,0
-	is cs
-	ldi r3,128
-	fi
-	add r2, r1
-	st r0,r1
-	move r3,r2
-	pop r3
-	inc r0
-	dec r3
+if
+ld r0, r1
+shr r1
+push r3
+ldi r3,0
+is cs
+ldi r3,128
+fi
+add r2, r1
+st r0,r1
+move r3,r2
+pop r3
+inc r0
+dec r3
 wend
 halt
-
 asect 0x3C
 ENDINPUTS>
 arr: 
